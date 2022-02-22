b23a7956362d6c833a201d5263d40de6a08bb728
Coudylywane <lywanecoudy@gmail.com>
2022-02-22 09:47:06 +0100

    lister

--- templates/etudiant/liste.etudiant.html.php
+++ templates/etudiant/liste.etudiant.html.php
@@ -1,9 +1,21 @@
- 
-
-  <div class="container mt-5 ">
-  <table class="table">
-  <thead>
-    <tr>
+<div class="tout">
+<div class="filtre">
+  <label for="">Chambre</label>
+  <select class="form-control ml-4" name="etat" id="">
+    <option value="valider">Une personne</option>
+    <option value="en cour">Deux personnes</option>
+  </select>
+</div>
+<div class="filtre">
+  <label for="">Bourse</label>
+  <select class="form-control ml-4" name="etat" id="">
+    <option value="valider">Demie bourse</option>
+    <option value="en cour">Bourse entiere</option>
+  </select>
+</div>
+<div class="tables">
+  <table>
+  <tr>
       <th scope="col">Nom</th>
       <th scope="col">Prenom</th>
       <th scope="col">Login</th>
@@ -11,23 +23,23 @@
       <th scope="col">Adresse</th>
 
     </tr>
-  </thead>
-  <tbody>
-<?php foreach ($users as $user):?>
+    <?php foreach ($users as $user):?>
  
-    <tr>
-      <td><?=$user->nom?></td>
-      <td><?=$user->prenom?></td>
-      <td><?=$user->email?></td>
-      <td><?=$user->date_naissance?></td>
-      <td><?=$user->adresse?></td>
+ <tr>
+   <td><?=$user->nom?></td>
+   <td><?=$user->prenom?></td>
+   <td><?=$user->email?></td>
+   <td><?=$user->date_naissance?></td>
+   <td><?=$user->adresse?></td>
 
-    </tr>
+ </tr>
 <?php endforeach ?>   
-    
-  </tbody>
-</table>
-  </div>
+  </table>
+</div>
+</div>
+
+
+
 
   
 
